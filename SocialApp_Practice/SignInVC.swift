2b9d0bd53498efc7cdaa3a6996a569c90a483ce3
mmerani <michaelmerani@michaels-mbp.attlocal.net>
2017-01-25 15:35:06 -0500

    Facebook authorization completed

--- SocialApp_Practice/SignInVC.swift
+++ SocialApp_Practice/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,30 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("MIKE: Unable to authenticate with Facebook")
+            } else if result?.isCancelled == true {
+                print("MIKE: User canceled Facebook authentication")
+            } else {
+                print("MIKE: Authenticated")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("MIKE: Unable to authenticate")
+            } else {
+                print("MIKE: Successful authentication")
+            }
+        })
+    }
 
 }
 
